e6ff3867af534248f7b47d8728f7f874b56e354f
dave doolin <david.doolin@gmail.com>
2018-08-12 12:37:12 -0700

    video 127 connect to getr posts, has a redux bug, wont execute

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
+// http://reduxblog.herokuapp.com/api/posts?key=dave123
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=foobarski';
 
 export function fetchPosts() {
-    const request = axios.get(`${ROOT_URL}/api/posts${API_KEY}`);
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
     return {
       type: FETCH_POSTS,
