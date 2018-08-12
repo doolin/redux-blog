423942b48a6a79fc6fe98dba8186ffe19215ea38
dave doolin <david.doolin@gmail.com>
2018-08-12 14:39:08 -0700

    video 128 post listing component

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'fetch_posts';
 
 // http://reduxblog.herokuapp.com/api/posts?key=dave123
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=foobarski';
+const API_KEY = '?key=dave123';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
